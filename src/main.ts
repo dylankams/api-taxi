4c70e92cf89ecfa4a93926fe89c9ee2070f08900
dylankams <dylankam989@gmail.com>
2024-07-14 09:55:38 +0200

    commit 3

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import serverless from 'serverless-http';
+import { Request, Response } from 'express';
+import { INestApplication } from '@nestjs/common';
 
+dotenv.config();
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  
-
 
   // Enable CORS
   app.enableCors({
-    origin: '*', 
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3001);
+
+  return app;
 }
-bootstrap();
+
+export const handler = async (req: Request, res: Response) => {
+  const app: INestApplication = await bootstrap();
+  const server = serverless(app.getHttpServer());
+  return server(req, res);
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap();
+}
